ce6d688fbe3e7508d93abc55eac6fcebb7cc3e8b
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-13 08:58:08 +0200

    add styling to the header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -15,3 +15,25 @@
       </ul>
     </nav>
   </header>
+
+  <style>
+    header {
+      padding: 1rem;
+      background: lightskyblue;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+    
+    ul {
+      margin: 0;
+      list-style-type: none;
+      display: flex;
+      gap: 1rem;
+    }
+    
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+    </style>
