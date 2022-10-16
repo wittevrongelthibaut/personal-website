381fa88cd6a53448c3103cc2080b701182174079
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-16 20:00:31 +0200

    add layout for pages to fade in/out

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
   import Header from '$lib/components/Header.svelte'
   import '$lib/styles/style.scss'
+  import { fade } from 'svelte/transition'
+  
+  export let data
 </script>
 
 <Header />
 
-<main>
-  <slot />
-</main>
+{#key data.currentRoute}
+  <main in:fade={{ duration: 150, delay: 200 }} out:fade={{ duration: 200 }}>
+    <slot />
+  </main>
+{/key}
 
 <footer>Hello, I'm the footer.</footer>
