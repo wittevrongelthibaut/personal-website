60dcbd178d868b3ef0769a4e559fec6b44f9b5b9
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-16 20:19:52 +0200

    add an og:title

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,14 @@
 <script>
     export let data;
-  </script>
+</script>
+
+<svelte:head>
+  <title>{data.title} - Thibaut Wittevrongel blog</title>
+  <meta property="og:title" content={data.title} />
+</svelte:head>
   
-  <article>
-    <h1>{ data.title }</h1>
-    <p>Published: {data.date}</p>
-    <svelte:component this={data.content} />
-  </article>
+<article>
+  <h1>{ data.title }</h1>
+  <p>Published: {data.date}</p>
+  <svelte:component this={data.content} />
+</article>
