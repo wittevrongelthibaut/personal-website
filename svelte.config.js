8315bb172cd7627a7afbefe44ffc6553698b3822
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-16 20:04:11 +0200

    add postcss plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import autoprefixer from 'autoprefixer'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +10,14 @@ const config = {
 	},
 	extensions: ['.svelte', '.md'],
 	preprocess: [
-		sveltePreprocess(),
+		sveltePreprocess({
+			postcss: {
+			  plugins: [autoprefixer]
+			}
+		}),
 		mdsvex({
 			extensions: ['.md']
-		  })
+		})
 	  ]
 };
 
