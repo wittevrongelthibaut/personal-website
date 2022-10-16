4603e1b7c51bcff02405373b7ce8cde43f82f8e4
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-16 17:34:59 +0200

    show all the posts on the /blog endpoint

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Blog</h1>
-
-<p>My blog posts will go here eventually…</p>
+    <script>
+        export let data
+    </script>
+    
+    <h1>Blog</h1>
+      
+    <ul>
+      {#each data.posts as post}
+        <li>
+          <h2>
+            <a href={post.path}>
+              {post.meta.title}
+            </a>
+          </h2>
+          Published {post.meta.date}
+        </li>
+      {/each}
+    </ul>
