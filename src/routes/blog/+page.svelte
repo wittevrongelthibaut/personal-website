2065022f476a5a03be5329f28d7c58fb5098aae5
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-17 15:01:21 +0200

    add more styling to the blog endpoint

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,6 @@
     <script>
-	import Card from './../../lib/components/Card.svelte';
-	import { dateFormater } from '$lib/utils/index.js';
+	      import Card from './../../lib/components/Card.svelte';
+	      import { dateFormater } from '$lib/utils/index.js';
         export let data
     </script>
     
@@ -13,3 +13,16 @@
         </li>
       {/each}
     </ul>
+
+    <style>
+      h1{
+        font-family: headings;
+      }
+
+      ul{
+          display: grid;
+          grid-template-columns: repeat(auto-fill,minmax(30rem,1fr));
+          gap: 2rem;
+          padding: 0;
+       }
+    </style>
