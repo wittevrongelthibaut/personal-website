459d40b98f7fdd1cf3e231dee46ed56cc7511469
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-17 22:50:19 +0200

    change welcome text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<article>
+    <h1>Welcome to my site!</h1>
+    <p>Hello my name is Thibaut Wittevrongel and i would like to welcome you to my site!</p>
+    <p>On this site you can my blog and a little bit of information about me.</p>
+    <p>The site is still under development so don't panic if some little bugs pop-up or if anything changes later.</p>
+    <p>Have fun browsing on my site!</p>
+</article>
+
+<style>
+    article{
+       width: 50%;
+       margin: auto;
+    }
+    h1{
+        font-size: 2rem;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+    p{
+        line-height: 1.5rem;
+    }
+</style>
