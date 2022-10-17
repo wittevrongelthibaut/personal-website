d2709833bcc5ccc01c606194b8b3b8ae8bf65f91
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-17 15:01:33 +0200

    add styling for the card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,12 +1,31 @@
 <script>
     export let post;
+
+    const topColor = post.meta.topColor[0]
 </script>
 
-<article>
+<article style="--top-color: {topColor}">
     <a href={post.path}>
         <h3>{post.meta.tags}</h3>
         <h2>{post.meta.title}</h2>
         <p>{post.meta.description}</p>
-        <img src={post.meta.cover} alt={post.meta.cover_description}>
+        <img src={post.meta.cover} alt={post.meta.coverDescription}>
     </a>
 </article>
+
+<style>
+    article{
+        border-top: 2px solid var(--top-color);
+        box-shadow: 0 2px 4px -1px rgba(0,0,0,0.2),0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12);
+        padding: 1rem;
+        margin-bottom: 1rem;
+    }
+    h2{
+        font-size: 2rem;
+        font-weight: 700;
+    }
+    h3{
+        font-family: 'light';
+        color: var(--top-color);
+    }
+</style>
