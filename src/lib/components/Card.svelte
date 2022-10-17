88267e92de2f0081ba4dd40a4c7426cb58c99bf6
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-17 12:49:33 +0200

    create html for card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let post;
+</script>
+
+<article>
+    <a href={post.path}>
+        <h3>{post.meta.tags}</h3>
+        <h2>{post.meta.title}</h2>
+        <p>{post.meta.description}</p>
+        <img src={post.meta.cover} alt={post.meta.cover_description}>
+    </a>
+</article>
