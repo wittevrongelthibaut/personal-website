ed8c85fc5994e7efc91475d442bcd4a8fc8629c6
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-17 22:14:49 +0200

    add animations for cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,17 +1,28 @@
 <script>
+    import { fly } from 'svelte/transition';
+    import { backOut } from 'svelte/easing';
+
     export let post;
+    export let i;
 
     const topColor = post.meta.topColor[0]
 </script>
 
-<article style="--top-color: {topColor}">
-    <a href={post.path}>
-        <h3>{post.meta.tags}</h3>
-        <h2>{post.meta.title}</h2>
-        <p>{post.meta.description}</p>
-        <img src={post.meta.cover} alt={post.meta.coverDescription}>
-    </a>
-</article>
+<li in:fly={{
+    y: 100,
+    x: 0,
+    delay: 200 * i,
+    easing: backOut
+}}>
+    <article style="--top-color: {topColor}">
+        <a href={post.path}>
+            <h3>{post.meta.tags}</h3>
+            <h2>{post.meta.title}</h2>
+            <p>{post.meta.description}</p>
+            <img src={post.meta.cover} alt={post.meta.coverDescription}>
+        </a>
+    </article>
+</li>
 
 <style>
     article{
@@ -19,13 +30,23 @@
         box-shadow: 0 2px 4px -1px rgba(0,0,0,0.2),0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12);
         padding: 1rem;
         margin-bottom: 1rem;
+        background-color: #1c1b1b;
+    }
+
+    article:hover{
+        transform: scale(1.04);
     }
     h2{
         font-size: 2rem;
         font-weight: 700;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
     }
     h3{
         font-family: 'light';
         color: var(--top-color);
+    }
+    p{
+        margin-bottom: 1rem;
     }
 </style>
