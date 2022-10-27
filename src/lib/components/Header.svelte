06c3d294951f912304907420f95bb85006f28104
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2022-10-27 22:48:12 +0200

    style header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,13 @@
-
+<script>
+  import { page } from '$app/stores';
+</script>
 <header>
-    <a href="/">Home</a>
-  
     <nav>
       <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        {#if $page.url.pathname == '/'}
         <li>
           <a href="/#about">About</a>
         </li>
@@ -13,19 +17,19 @@
         <li>
           <a href="/#contact">Contact</a>
         </li>
+        {/if}
         <li>
           <a href="/blog">Blog</a>
         </li>
       </ul>
     </nav>
+    
   </header>
 
-  <style>
+  <style lang='scss'>
+    $accentColor: #B0DDC2;
     header {
       padding: 1rem;
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
       width: 100%;
     }
     
@@ -33,11 +37,26 @@
       margin: 0;
       list-style-type: none;
       display: flex;
-      gap: 1rem;
+      li{
+        border-left: 1px solid $accentColor;
+        border-right: 1px solid $accentColor;
+        padding: 0.5rem;
+        margin-left: -1px;
+      }
+      li:nth-last-child(1) {
+        margin-left: auto;
+      }
+
     }
     
     a {
       text-decoration: none;
       color: inherit;
+      font-size: 1.2rem;
+      font-weight: 700;
+      padding: 1rem;
+      &:hover {
+        color: $accentColor;
+      }
     }
     </style>
