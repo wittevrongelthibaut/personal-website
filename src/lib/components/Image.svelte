8f632eec1f5399d4a662e35edd808c4f249063bb
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-04-06 14:25:50 +0200

    add image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let imgUrl = '';
+	export let altText = '';
+	export let size = 'large';
+</script>
+
+<figure>
+	<img class={size} src={imgUrl} alt={altText} />
+	<figcaption>{altText}</figcaption>
+</figure>
+
+<style>
+	figure {
+		margin: 4% auto;
+	}
+	figcaption {
+		font-size: 0.8rem;
+		color: #666;
+		text-transform: capitalize;
+	}
+	img {
+		border-radius: 0.375rem;
+	}
+	.max-size {
+		width: 100% !important;
+		object-fit: cover;
+	}
+	.large {
+		max-width: 100%;
+	}
+	.medium {
+		width: 75%;
+	}
+	.small {
+		width: 50%;
+	}
+</style>
