ad6fa57c36999b7dca2bee971363713285754bfa
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-05-20 08:48:59 +0200

    create component to use links in my blogposts

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let url;
+</script>
+
+<a href="{url}" target="_blank">
+    <slot />
+</a>
+
+<style lang="scss">
+    $accentColor: #B0DDC2;
+    a{
+        display: inline-block;
+        position: relative;
+        color: $accentColor ;
+        &:after{
+            content: '';
+            position: absolute;
+            width: 100%;
+            transform: scaleX(0);
+            height: 2px;
+            bottom: 0;
+            left: 0;
+            background-color: $accentColor;
+            transform-origin: bottom right;
+            transition: transform 0.25s ease-out;
+        }
+        &:hover:after{
+            transform: scaleX(1);
+            transform-origin: bottom left;
+        }
+    }
+</style>
